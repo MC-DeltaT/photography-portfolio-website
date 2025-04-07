59a0191f583e44bebfdc59868e195c2808c635e2
Reece Jones <reece.jones131@gmail.com>
2025-04-07 18:50:47 +1000

    Allow only ingest or only build

--- source/buildtool/__main__.py
+++ source/buildtool/__main__.py
@@ -15,12 +15,16 @@ def main() -> None:
     arg_parser = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
-    arg_parser.add_argument('-i', '--ingest', type=Path, default=Path('../ingest'), help='Directory to ingest new photos from')
-    arg_parser.add_argument('-d', '--data', type=Path, default=Path('./data'), help='Directory containing source data')
-    arg_parser.add_argument('-o', '--output', type=Path, default=Path('../site'), help='Directory to build site into')
+    arg_parser.add_argument('-i', '--ingest-path', type=Path, default=Path('../ingest'), help='Directory to ingest new photos from')
+    arg_parser.add_argument('-d', '--data-path', type=Path, default=Path('./data'), help='Directory containing source data')
+    arg_parser.add_argument('-o', '--output-path', type=Path, default=Path('../site'), help='Directory to build site into')
     arg_parser.add_argument('-v', '--verbose', action='store_true', help='Log more')
-    arg_parser.add_argument('--dry-run', action='store_true', help='Simulate actions without writing anything')
-    arg_parser.add_argument('--fast', action='store_true', help='Make the build faster by taking shortcuts (for testing only)')
+    build_mode_group = arg_parser.add_mutually_exclusive_group()
+    build_mode_group.add_argument('--dry-run', action='store_true', help='Simulate actions without writing anything')
+    build_mode_group.add_argument('--fast', action='store_true', help='Make the build faster by taking shortcuts (for testing only)')
+    actions_group = arg_parser.add_mutually_exclusive_group()
+    actions_group.add_argument('--ingest', action=argparse.BooleanOptionalAction, default=None, help='Ingest photos')
+    actions_group.add_argument('--build', action=argparse.BooleanOptionalAction, default=None, help='Build the site')
 
     args = arg_parser.parse_args()
 
@@ -29,14 +33,19 @@ def main() -> None:
         # PIL spams debug logs we don't need
         logging.getLogger('PIL').setLevel(logging.INFO)
         logger.info('DRY RUN - won\'t write output')
-    
-    if args.dry_run and args.fast:
-        arg_parser.error('fast should not be used with dry_run')
 
-    if args.ingest.exists():
-        run_ingest(args.ingest, args.data, dry_run=args.dry_run)
+    if args.ingest is None and args.build is None:
+        ingest = True
+        build = True
+    else:
+        ingest = bool(args.ingest)
+        build = bool(args.build)
 
-    run_build(args.output, args.data, fast=args.fast, dry_run=args.dry_run)
+    if ingest and args.ingest_path.exists():
+        run_ingest(args.ingest_path, args.data_path, dry_run=args.dry_run)
+
+    if build:
+        run_build(args.output_path, args.data_path, fast=args.fast, dry_run=args.dry_run)
 
 
 if __name__ == '__main__':
